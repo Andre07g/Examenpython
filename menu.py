0e9e1f422864b13715923233f29a9fa18c3278bd
Andre07g <106554013+Andre07g@users.noreply.github.com>
2025-03-27 08:02:02 -0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,50 @@
+import time
+import os
+
+def limpiar():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def saludo():
+    print("="*50)
+    print(" "*20,"Limeware")
+    print("="*50)
+    print("Bienvenido al sistema de artistas")
+    time.sleep(1)
+    print("Cargando sistema...")
+    time.sleep(2)
+    print("Sistema cargado correctamente")
+    time.sleep(2)
+    limpiar()
+
+def menuprincipal():
+    menupr="""
+    *******Menu Principal*******
+    1.Artistas
+    2.Generos
+    3.Paises
+    4.Salir
+    """
+    print(menupr)
+    return input("Ingrese una opciòn: ")
+
+def generosmenu():
+    menugen="""
+    *****Generos******
+    1.Añadir genero
+    2.Eliminar genero
+    3.Consultar codigo genero
+    4.Salir
+    """
+    print(menugen)
+    return input("Ingrese una opcion: ")
+
+def paisesmenu():
+    menupai="""
+    ******Paises*******
+    1.Añadir pais
+    2.Eliminar pais
+    3.Consultar codigos pais
+    4.Salir
+    """
+    print(menupai)
+    return input("Ingrese una opcion: ")
